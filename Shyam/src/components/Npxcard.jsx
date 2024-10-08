6628aed3f9faaeb3600d8695784d2876b3d646d1
Shyamkadiwar <shyamkadiwar17@gmail.com>
2024-10-08 10:18:49 +0530

    fixed bug

--- Shyam/src/components/Npxcard.jsx
+++ Shyam/src/components/Npxcard.jsx
@@ -14,7 +14,7 @@ const Npxcard = () => {
 
   return (
     <div className="flex items-center bg-[#1e1e2d] py-2 pr-2 pl-1 mt-9 rounded-2xl max-w-52 shadow-lg border border-slate-800">
-      <div className="bg-[#2bc5e0] w-5 h-11 rounded-xl">.</div>
+      <div className="bg-[#2bc5e0] w-5 h-11 rounded-xl"> </div>
       <div className="flex-1 px-3 text-[#2ccae6] font-mono sm:text-lg text-md">
         Download CV
       </div>
